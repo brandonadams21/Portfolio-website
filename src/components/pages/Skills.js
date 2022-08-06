73ebd53294015f89e3b3460949d3946a46bdf262
Brandon Adams <brandonadams@Brandons-MacBook-Pro.local>
2022-08-06 15:15:29 -0400

    Skills and about me WIP

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,40 +1,62 @@
 import React from "react";
 import './Skills.css'
+import { SiTypescript, SiJavascript, SiHtml5, SiCss3, SiReact, SiGit, SiStyledcomponents, SiSass, SiPostgresql, SiHeroku } from 'react-icons/si';
+import { DiSass } from 'react-icons/di'
 
 function Skills() {
+  
   return (
     <>
-   <div id="sphere">
-  <div class="circ" style={{color: 'white'}}>aaaaa</div>
-  <div class="circ" style={{color: 'white'}}>bbbbb</div>
-  <div class="circ" style={{color: 'white'}}>ccccc</div>
-  <div class="circ" style={{color: 'white'}}>ddddd</div>
-  <div class="circ" style={{color: 'white'}}>eeeee</div>
-  <div class="circ" style={{color: 'white'}}>ffffffff</div>
-</div>
-
-      <h1 style={{ color: "rgb(81, 193, 241)", fontSize: '50px', float: 'left', marginLeft: '40px' }}>Skills & Experience</h1>
-      <br/>
-      <p style={{color: 'white', width: '50%', height: '50%', fontSize: '25px', paddingTop: '100px', marginLeft: '40px'}}>I started my coding journey in August of 2021. During that time I did not think I would be 
-      creating this website so early in my career. Currently I am doing remote work for Joint Special Operations Command and have been doing that
-      since April of 2022. 
+     <div class="card">
+    <h1>Skills and Technologies
+      <div class="scroller">
+        
+        <span>
+        
+        <span className='span2'><SiTypescript/></span><br/>
+        <span className='span3'><SiHtml5/></span><br/>
+        <span className='span4'><SiCss3/></span><br/>
+        <span className='span5'><DiSass/></span><br/>
+        <span className='span6'><SiReact/></span><br/>
+        <span className='span7'><SiGit/></span>
+        </span>
+      </div>
+    </h1>
+  </div>
+  <div className="scroller"></div>
+      <p style={{color: 'white', width: '50%', height: '50%', fontSize: '25px', paddingTop: '100px', marginLeft: '40px'}}>
+        Languages
       </p>
+      <span className='span1'><SiJavascript/></span>
+      <span className='span1'><SiTypescript/></span>
       <br/>
-      <p style={{color: 'white', width: '50%', height: '50%', fontSize: '25px', marginLeft: '40px'}}>
-        During my time with JSOC I have worked on team creating an app specifed towards mission effiency. 
-        I have worked with JavaScript, TypeScript, HTML, CSS, and GoLang. We have built that application using TDD as our main focus.
+      <div className="scroller1">
+      <p style={{color: 'white', width: '50%', height: '50%', fontSize: '25px', paddingTop: '100px', marginLeft: '40px'}}>
+        Front-End
       </p>
-      <br/>
-      <p style={{color: 'white', width: '50%', height: '50%', fontSize: '25px', marginLeft: '40px'}}>
-          After my time with JSOC I will be working for the 18th ABN Corps of the United States Army.
-          Shortly after that time I will be starting my civilian carrer. I am currently enrolled and taking classes at 
-          the Universty of Penn State. I am studying to become a software engineer.
+      <span className='span1'><SiHtml5/></span>
+      <span className='span1'><SiCss3/></span>
+      <span className='span1'><SiReact/></span>
+      <span className='span1'><SiStyledcomponents/></span>
+      <span className='span1'><SiSass/></span>
+      </div>
+      <div className="scroller1">
+      <p style={{color: 'white', width: '50%', height: '50%', fontSize: '25px', paddingTop: '100px', marginLeft: '40px'}}>
+        Back-End
       </p>
-      <br/>
+      <span className='span1'><SiPostgresql/></span>
+      <i class="fab fa-golang"></i>
+      </div>
+      <div className="scroller1">
+      <p style={{color: 'white', width: '50%', height: '50%', fontSize: '25px', paddingTop: '100px', marginLeft: '40px'}}>
+        Tools
+      </p>
+      <span className='span1'><SiGit/></span>
+      <span className='span1'><SiHeroku/></span>
+      </div>
       <p  style={{color: 'white', width: '50%', height: '50%', fontSize: '25px', marginLeft: '40px', marginBottom: '50px'}}>
           Visit my <a style={{color: 'rgb(81, 193, 241)'}} href="https://www.linkedin.com/in/brandon-adams-93966b239">LinkedIn</a> and <a style={{color: 'rgb(81, 193, 241)'}} href="/contact">contact</a> me for more details!
       </p>
-      
     </>
   );
 }
