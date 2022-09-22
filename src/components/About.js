f34d200b1c383526cdce1ae7aa650484e320ec7d
Brandon Adams <brandonadams@Brandons-MBP.lan>
2022-09-22 12:33:29 -0500

    smooth scroll added when contact is clicked

--- src/components/About.js
+++ src/components/About.js
@@ -4,17 +4,17 @@ import "./About.css";
 
 function About() {
   return (
-    <div className="about">
+    <div className="about" id="aboutMe">
       <h1 className="aboutHeader">About me</h1>
       <p style={{ paddingTop: "10px" }}>
         Full stack developer. From Michigan,
         based in Tennessee.
       </p>
-      <h2 className="experienceHeader" style={{paddingTop: '25px'}}>Experience</h2>
+      <h2 className="experienceHeader" style={{paddingTop: '50px'}}>Experience</h2>
       <p style={{paddingTop: '10px'}}>United States Army Infantry</p>
       <p style={{paddingTop: '10px'}}>Joint Special Operations Command</p>
       <p style={{paddingTop: '10px'}}>United States Army Data Warfare</p>
-      <h2 className="educationHeader" style={{paddingTop: '25px'}}>Education</h2>
+      <h2 className="educationHeader" style={{paddingTop: '50px'}}>Education</h2>
       <p style={{paddingTop: '10px'}}>Galvanize SDI</p>
       <p style={{paddingTop: '10px'}}>Universty of Penn State 2022-Current</p>
     </div>
